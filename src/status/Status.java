d60b01930d2c2d22d6b2e0ee0a3192d8a1c1bf9e
Gurcharan11 <sin11682@sheridancollege.ca>
2022-06-23 10:53:30 -0400

    Merge origin/master
    Conflicts:
    	src/cardtrickice1/CardTrickICE1.java

--- src/status/Status.java
+++ src/status/Status.java
@@ -24,10 +24,9 @@ public class Status {
     public static void main(String[] args)
     {
     Scanner in =new Scanner(System.in);
-    System.out.println("Enter the user status code (zero,one,two,three) in string");
-    String code = in.next();
-    StausUser t= new StausUser();
-    t.statusDetail(code); 
+    System.out.println("Enter the user status code (0,1,2,3) in int");
+    Status code = in.next();
+    StausUser t= new StausUser(code);
     }
     
 }
--- src/status/StausUser.java
+++ src/status/StausUser.java
@@ -7,28 +7,52 @@ package status;
 
 /**
  * MidTerm-Exam
+ *
  * @author srinivsi
  */
-public class StausUser 
-{
-   public void statusDetail(String code)
-{
-switch(code.toUpperCase())
-        {
-        case "ZERO": System.out.println("REJECTED");
-        break;
-        case "ONE": System.out.println("PENDING");
-        break;
-        case "TWO":
-        System.out.println("PROCESSING");
-        break;
-        case "THREE": 
-            System.out.println("APPROVED");
-        break;
-        default:
-         System.out.println("NOT VALID CODE");
-        break;
+public class StausUser {
+
+    
+    public enum Status {ZERO, ONE, TWO, THREE};
+    private Status code;
+
+    public StausUser(Status code) {
+        this.code = code;
+    }
+
+    public void statusDetail(Status code) {
+
+        switch (getCode()) {
+            case ZERO:
+                System.out.println("REJECTED");
+                break;
+            case ONE:
+                System.out.println("PENDING");
+                break;
+            case TWO:
+                System.out.println("PROCESSING");
+                break;
+            case THREE:
+                System.out.println("APPROVED");
+                break;
+            default:
+                System.out.println("NOT VALID CODE");
+                break;
         }
-}
- 
+    }
+
+    /**
+     * @return the code
+     */
+    public Status getCode() {
+        return code;
+    }
+
+    /**
+     * @param code the code to set
+     */
+    public void setCode(Status code) {
+        this.code = code;
+    }
+
 }
